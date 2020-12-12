c4a56da9d119b6cc09eb14e668813fc4286eca83
Macenna <macennacowen@gwu.edu>
2020-12-12 13:22:21 -0500

    16 - Working on

---
+++ 01-Activities/16-NYTSearch/Instructions/Unsolved/script.js
@@ -0,0 +1,12 @@
+$.ajax({
+    type: "GET",
+    url: "queryURL",
+    success: function (response) {
+        console.log(startDate);
+        console.log(endDate);
+        console.log(input);
+        console.log(numberOfArticles);
+    }
+});
+
+
